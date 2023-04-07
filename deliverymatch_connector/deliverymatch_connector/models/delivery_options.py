bc1ee3e733862025f7f712e11dfa24525ad5ce30
Bertjan-dooit <56160472+Bertjan-dooit@users.noreply.github.com>
2023-04-07 10:46:01 +0200

    Delete deliverymatch_connector directory

--- deliverymatch_connector/deliverymatch_connector/models/delivery_options.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class DeliveryOptions(models.Model):
-    _name = "delivery.options"
-    _description = "handles the delivery options"
-    carrier_name = fields.Char(string="Carrier name")
-    
