43d0ec319d82ff58d10b3dbd0506fbe7900f571a
Bertjan-dooit <56160472+Bertjan-dooit@users.noreply.github.com>
2023-07-07 10:13:30 +0200

    Add files via upload

---
+++ dmmodule/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from . import sale_order
+from . import product
+from . import dm_delivery_options
+from . import config
+from . import stock_picking
+from . import stock_warehouse
+from . import operation_type
+from . import customer_partners
+# from . import sale_order_line
