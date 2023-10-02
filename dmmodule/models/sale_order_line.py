62d4657e7c0432a86f8e4d781979ac695da6b4f9
Bertjan-dooit <56160472+Bertjan-dooit@users.noreply.github.com>
2023-10-02 18:34:09 +0200

    Add files via upload

---
+++ dmmodule/models/sale_order_line.py
@@ -0,0 +1,7 @@
+# from odoo import api, fields, models, _, exceptions
+
+# class SaleOrderLine(models.Model):
+#     # _logger = logging.getLogger("DeliveryMatch - SaleOrder")
+#     _inherit = "sale.order.line"
+    
+#     x_studio_length = fields.Integer(string="Lengte", default=0)
