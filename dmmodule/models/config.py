0b80636cef5848be40d85704ea71655cb4a089a9
Bertjan-dooit <56160472+Bertjan-dooit@users.noreply.github.com>
2024-03-20 11:33:36 +0100

    Add files via upload

---
+++ dmmodule/models/config.py
@@ -0,0 +1,89 @@
+from odoo import models, fields
+
+class Config(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    base_url = fields.Char(string="DeliveryMatch base URL *", required=True)
+    api_key = fields.Char(string="API Key *", required=True)
+    client_id = fields.Char(string="Client ID *", required=True)
+    override_length = fields.Boolean(string="Take product-length from order line", default=False)
+
+    # this hides the "SEND ORDER TO WAREHOUSE" button in sale order level.
+    so_hide_book_hub_btn = fields.Boolean(string="Hide 'Send order to warehouse' button", default=False)
+
+    # this hides the "BOOK TO CARRIER" button in sale order level.
+    so_hide_book_carrier_btn = fields.Boolean(string="Hide 'Book to carrier' button", default=False)
+
+    sale_order_as_draft = fields.Boolean(string="Insert ALL SHIPMENTS with status 'DRAFT'.", default=False)
+    # IF True get scheduled_date from Stock.Picking and set delivery_date in shipment
+    determine_delivery_date = fields.Boolean(string="Determine delivery date", default=False)
+    calculate_packages = fields.Boolean(string="Calculate packages based on product packages", default=False)
+
+
+
+    delivery_option_preference = fields.Selection(
+        [
+            ("lowest", "Lowest price"),
+            ("earliest", "Earliest date"),
+            (
+                "most_green",
+                "Greenest delivery (Only possible in accordance with Big Mile)",
+            ),
+            ("nothing", "No auto selection"),
+        ],
+        required=True,
+        string="Auto select delivery option preference *",
+    )
+
+    def get_values(self):
+        res = super(Config, self).get_values()
+        res['override_length'] = self.env["ir.config_parameter"].sudo().get_param("dmmodule.override_length", default=False)
+
+        res.update(
+            api_key=self.env["ir.config_parameter"].sudo().get_param("dmmodule.api_key", default=None),
+            delivery_option_preference=self.env["ir.config_parameter"].sudo().get_param("dmmodule.delivery_option_preference", default="lowest"),
+            client_id=self.env["ir.config_parameter"].sudo().get_param("dmmodule.client_id", default=None),
+            base_url=self.env["ir.config_parameter"].sudo().get_param("dmmodule.base_url", default=None),
+            so_hide_book_hub_btn=self.env["ir.config_parameter"].sudo().get_param("dmmodule.so_hide_book_hub_btn", default=False),
+            so_hide_book_carrier_btn=self.env["ir.config_parameter"].sudo().get_param("dmmodule.so_hide_book_carrier_btn", default=False),
+            sale_order_as_draft=self.env["ir.config_parameter"].sudo().get_param("dmmodule.sale_order_as_draft", default=False),
+            determine_delivery_date=self.env["ir.config_parameter"].sudo().get_param("dmmodule.determine_delivery_date", default=False),
+            calculate_packages=self.env["ir.config_parameter"].sudo().get_param("dmmodule.calculate_packages", default=False)
+
+        )
+        return res
+
+    def set_values(self):
+        super(Config, self).set_values()
+        self.env["ir.config_parameter"].sudo().set_param("dmmodule.api_key", self.api_key)
+        self.env["ir.config_parameter"].sudo().set_param("dmmodule.delivery_option_preference", self.delivery_option_preference)
+        self.env["ir.config_parameter"].sudo().set_param("dmmodule.client_id", self.client_id)
+        self.env["ir.config_parameter"].sudo().set_param("dmmodule.base_url", self.base_url)
+        self.env["ir.config_parameter"].sudo().set_param("dmmodule.override_length", self.override_length)
+        self.env["ir.config_parameter"].sudo().set_param("dmmodule.so_hide_book_carrier_btn",self.so_hide_book_carrier_btn)
+        self.env["ir.config_parameter"].sudo().set_param("dmmodule.so_hide_book_hub_btn", self.so_hide_book_hub_btn)
+        self.env["ir.config_parameter"].sudo().set_param("dmmodule.sale_order_as_draft", self.sale_order_as_draft)
+        self.env["ir.config_parameter"].sudo().set_param("dmmodule.determine_delivery_date", self.determine_delivery_date)
+        self.env["ir.config_parameter"].sudo().set_param("dmmodule.calculate_packages", self.calculate_packages)
+
+        self.update_sale_orders()
+
+    def update_sale_orders(self):
+        sale_orders = self.env["sale.order"].search([])
+        
+        for sale_order in sale_orders:
+            sale_order.write({"hide_carrier_btn": self.so_hide_book_carrier_btn})
+            sale_order.write({"hide_hub_btn": self.so_hide_book_hub_btn})
+
+    def select_warehouses_for_hub(self):
+        view_id = self.env.ref("dmmodule.view_warehouse_list").id
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Select one or more warehouses to assign as external",
+            "res_model": "stock.warehouse",
+            "view_type": "tree",
+            "view_mode": "tree",
+            "view_id": view_id,
+            "domain": [],
+            "target": "new",
+        }
---
+++ dmmodule/models/order_handler.py
@@ -0,0 +1,80 @@
+import json, logging, traceback
+import requests
+import base64
+from .dm_api import DmApi
+from .helper import Helper
+from .deliverymatch_exception import DeliveryMatchException
+from .customer import Customer
+from .product import DmProducts
+from .shipping_option import *
+from .shipment import Shipment
+from odoo.tools import html2plaintext
+
+
+class OrderHandler:
+
+    def __init__(self, base_url, api_key, client_id):
+        self.api = DmApi(base_url, api_key, client_id)
+        self._logger = logging.getLogger("DeliveryMatch -- OrderHandler")
+
+    def set_channel_franco(self, is_franco):
+        if(is_franco == True):
+            self.api.set_channel("FRANCO")
+    
+    def get_shipping_options(self, shipment: Shipment, customer: Customer, products: DmProducts, packages = None):
+        try:
+            self.set_channel_franco(customer.is_franco)
+
+            self._logger.info("Fetching shipping options")
+            shipping_options = self.api.request_shipping_options(customer, shipment, products, packages=packages)
+            return shipping_options
+        
+        except DeliveryMatchException as e:
+            raise DeliveryMatchException(e)
+
+        except Exception as e:
+            self._logger.error(f"An error occurred while fetching the shipping options: {e}")
+            self._logger.error(traceback.format_exc())
+            raise Exception("An error occurred while fetching the shipping options")
+        
+    
+    def get_shipping_option_by_preference(self, shipment: Shipment, customer: Customer, products: DmProducts, preference, is_delivery= False, packages=None):
+        try:
+            self._logger.info("Fetching shipping option by preference")
+            self.set_channel_franco(customer.is_franco)
+
+             
+            shipping_options = self.api.request_shipping_options(customer, shipment, products, False, packages=packages)
+            shipping_option = self.api.get_shipping_option_by_preference(shipping_options, preference)
+            self.api.updateShipmentMethod(shipping_option.shipment_id, shipment.odoo_order_display_name, shipping_option.method_id,shipping_option.delivery_date)
+            return shipping_option
+        
+        except DeliveryMatchException as e:
+            raise DeliveryMatchException(e)
+        except Exception as e:
+            self._logger.error(f"An error occurred while fetching the shipping option by preference: {e}")
+            self._logger.error(traceback.format_exc())
+            raise Exception("An error occurred while fetching the shipping option by preference")
+        
+
+    
+    def book_shipment(self, shipment:Shipment, customer: Customer, products: DmProducts, is_delivery=False, packages=None):
+        try:
+            self._logger.info("Booking shipment")
+            self.set_channel_franco(customer.is_franco)
+            booking_response: dict = self.api.reqeust_book_shipment(customer, shipment, products, packages=packages)
+            # returns a dictionary with the following structure: tracking_url, shipment_label, booked_timestamp
+            return booking_response
+
+
+
+        except DeliveryMatchException as e:
+            raise DeliveryMatchException(e)
+        except Exception as e:
+            self._logger.error(f"An error occurred while booking the shipment: {e}")
+            self._logger.error(traceback.format_exc())
+            raise Exception("An error occurred while booking the shipment")
+
+
+
+
