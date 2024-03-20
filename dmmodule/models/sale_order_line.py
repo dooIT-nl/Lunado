3dd3dac511701e75390e83cd52d7545ca3ec9ec3
Bertjan-dooit <56160472+Bertjan-dooit@users.noreply.github.com>
2024-03-20 11:31:56 +0100

    Delete dmmodule directory

--- dmmodule/models/sale_order_line.py
+++
@@ -1,7 +0,0 @@
-# from odoo import api, fields, models, _, exceptions
-
-# class SaleOrderLine(models.Model):
-#     # _logger = logging.getLogger("DeliveryMatch - SaleOrder")
-#     _inherit = "sale.order.line"
-    
-#     x_studio_length = fields.Integer(string="Lengte", default=0)
