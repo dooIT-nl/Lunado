938ee8b559e9bf74ecdcf2a724b0117b2d5c9c50
Bertjan-dooit <56160472+Bertjan-dooit@users.noreply.github.com>
2024-05-06 18:48:33 +0200

    Delete dmmodule directory

--- dmmodule/models/stock_move_line.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models, api
-
-
-class StockMoveLine(models.Model):
-    _inherit = 'stock.move.line'
-    product_dm_hscode = fields.Char(string="HS-CODE", related="product_id.dm_hscode", copy=False, readonly=False)
