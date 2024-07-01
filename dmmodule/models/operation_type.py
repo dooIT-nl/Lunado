17048afff1deac63a83ca631590290b4c576630d
Bertjan-dooit <56160472+Bertjan-dooit@users.noreply.github.com>
2024-07-01 14:41:53 +0200

    Delete dmmodule directory

--- dmmodule/models/operation_type.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models, _
-
-class OperationType(models.Model):
-    _inherit = "stock.picking.type"
-    dm_warehouse_number = fields.Selection(string='Warehouse nr DeliveryMatch', related='warehouse_id.warehouse_options')
-
