26889a05a56a28d2f28b48b1d369409f3c857b07
Bertjan-dooit <56160472+Bertjan-dooit@users.noreply.github.com>
2024-07-04 12:56:06 +0200

    Delete dmmodule directory

--- dmmodule/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# import pydevd_pycharm
-# pydevd_pycharm.settrace('host.docker.internal', port=12345, stdoutToServer=True, stderrToServer=True)
-
-from . import models
-from . import wizard
-from . import controllers
--- dmmodule/models/product_packaging.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from odoo import fields, models, api
-
-class ProductPackaging(models.Model):
-    _inherit = "product.packaging"
-    _logger = logging.getLogger("product.packaging")
-
-    min_qty = fields.Float(string="Minimum Contained Quantity")
