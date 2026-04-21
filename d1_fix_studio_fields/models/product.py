158567633893cb753c3455be79b31545d4778620
Bertjan-dooit <56160472+Bertjan-dooit@users.noreply.github.com>
2026-04-21 13:16:43 +0200

    Update product.py

--- d1_fix_studio_fields/models/product.py
+++ d1_fix_studio_fields/models/product.py
@@ -3,6 +3,5 @@ from odoo import models, fields
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    x_studio_use_qty = fields.Boolean(
-        store=True
-    )
+    x_studio_use_qty = fields.Boolean(store=True)
+    x_studio_use_length = fields.Boolean(store=True)
